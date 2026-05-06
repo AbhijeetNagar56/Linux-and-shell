80e40043c1fce2ac7b964cc0b39e5efdb2c032f6
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-06 10:24:56 +0530

    day 2 commands in linux

---
+++ day_02.sh
@@ -0,0 +1,28 @@
+cat # options -r -n -b
+head # options -n -c
+tail # options -n -c
+
+wc # options -l -w -c
+sort # options -r -n -k
+uniq # options -c -d -u
+grep # options -i -v -n
+find # options -name -type -size
+chmod # options -R -v
+chown # options -R -v
+ln # options -s -v
+tar # options -cvf -xvf
+gzip # options -d -k
+gunzip # options -k
+zip # options -r -v
+unzip # options -l -v
+df # options -h -T
+du # options -h -s
+ps # options -aux -ef
+top # options -b -n
+kill # options -9 -15
+ping # options -c -i
+curl # options -I -L -o
+wget # options -O -q
+ssh # options -p -i
+scp # options -r -P
+rsync # options -avz -e
