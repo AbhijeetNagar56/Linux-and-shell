7b5456963d8a15589caef44c8d038d3aa7427c03
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-08 14:24:56 +0530

    day_04.sh

---
+++ day_04.sh
@@ -0,0 +1,5 @@
+sudo useradd <username> # -d
+sudo usermod <username> # sudo usermod -aG sudo username
+sudo userdel -r <username>
+sudo passwd -l <username> # locking -u to unlock
+# /etc/passwd /etc/shadow store account information
