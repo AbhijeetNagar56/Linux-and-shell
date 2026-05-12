b8a445fd318eb27c34d3bca9a45b3dd381728d88
AbhijeetNagar56 <nagarabhijeet0@gmail.com>
2026-05-12 15:28:18 +0530

    day 06 commands

---
+++ dayWise/day_06.sh
@@ -0,0 +1,12 @@
+echo $PATH
+which ls
+which -a ls
+VARNAME="val"
+echo $VARNAME
+export VARNAME # changed from private
+bash # start a new session
+
+nano ~/.zshrc
+# alias name='val'
+source ~/.zshrc
+
